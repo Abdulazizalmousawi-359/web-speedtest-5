3681eb85ed4b5f5f541e833f3cd1419cdf4a53e3
Abdulazizalmousawi-359 <66692273+Abdulazizalmousawi-359@users.noreply.github.com>
2020-07-01 17:01:08 +0300

    done

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,11 +1,17 @@
 let instructor = {
   name: "Menawer",
-  courses: ["JavaScript", "React", "PHP"],
+  courses: ["JavaScript", "React", "PHP"];
   githubAccount: {
     username: "careless10",
     numberOfRepos: 200,
   },
+  console.log(name);
+
+  console.log(instructor.courses[0]);
+  
+  console.log(username);
   sayHello: function () {
     console.log("Hi, I'm Menawer");
   },
 };
+
